shiptest-ss13/Shiptest

Boomers Ruin The Economy - Pt. 1: No More Free Materials (#619)
* reaganomics, baby

* mechfab adjustments

* whoops

* minor runtime fixes

* fixes exofab needing a linked account when one isn't needed.

* makes emags remove cost reqs, adds user credit balance to ORM

* bunch o' fixes

* rebuilds tgui

* reganomics two, electric trickledown

* a comparison

* Drone and borg fixes

Co-authored-by: Shayne Fitzgerald <robotech417@yahoo.com>
Co-authored-by: Yodeler <superjacob50@yahoo.com>

code/datums/components/material_container.dm
@@ -9,6 +9,8 @@
 		MAX_STACK_SIZE - size of a stack of mineral sheets. Constant.
 */
 
+#define MINIMUM_REFUND_THRESHOLD 5000
+
 /datum/component/material_container
 	var/total_amount = 0
 	var/max_amount
@@ -21,6 +23,12 @@
 	var/precise_insertion = FALSE
 	var/datum/callback/precondition
 	var/datum/callback/after_insert
+	/// The bank account that will recieve funds at certain times when mats are used/withdrawn.
+	var/datum/bank_account/linked_account
+	/// Modifier for all materials drawn when there's a linked account.
+	var/cost_modifier = 1
+	/// The minimum required amount of credits in a bank account to consider giving refunds for inserting materials. Currently the same as cargo gets roundstart.
+	var/refund_minimum = 5000
 
 /// Sets up the proper signals and fills the list of materials with the appropriate references.
 /datum/component/material_container/Initialize(list/mat_list, max_amt = 0, _show_on_examine = FALSE, list/allowed_types, datum/callback/_precondition, datum/callback/_after_insert, _disable_attackby)
@@ -101,6 +109,12 @@
 	var/inserted = insert_item(I, stack_amt = requested_amount)
 	if(inserted)
 		to_chat(user, "<span class='notice'>You insert a material total of [inserted] into [parent].</span>")
+		if(linked_account)
+			var/cost = get_material_list_cost(I.custom_materials) * (cost_modifier / 4) //25% refund default, scaling with the price of mats.
+			cost = FLOOR(clamp(cost, 0, linked_account.account_balance - refund_minimum), 1) //Will pay what it can, as long as it doesn't go under the minimum threshold.
+			var/obj/item/card/id/C = user.get_idcard(TRUE)
+			if(C?.registered_account)
+				C.registered_account.transfer_money(linked_account, cost)
 		qdel(I)
 		if(after_insert)
 			after_insert.Invoke(I, last_inserted_id, inserted)
@@ -147,12 +161,14 @@
 	return FALSE
 
 /// Uses an amount of a specific material, effectively removing it.
-/datum/component/material_container/proc/use_amount_mat(amt, var/datum/material/mat)
+/datum/component/material_container/proc/use_amount_mat(amt, var/datum/material/mat, datum/bank_account/using_account, charge = FALSE)
 	if(!istype(mat))
 		mat = SSmaterials.GetMaterialRef(mat)
 	var/amount = materials[mat]
 	if(mat)
 		if(amount >= amt)
+			if(charge && linked_account && !linked_account.transfer_money(using_account, get_material_cost(mat, amt), 1))
+				return FALSE
 			materials[mat] -= amt
 			total_amount -= amt
 			return amt
@@ -168,9 +184,9 @@
 		return T.transer_amt_to(src, -amt, mat)
 	var/tr = min(amt, materials[mat],T.can_insert_amount_mat(amt, mat))
 	if(tr)
-		use_amount_mat(tr, mat)
-		T.insert_amount_mat(tr, mat)
-		return tr
+		if(use_amount_mat(tr, mat))
+			T.insert_amount_mat(tr, mat)
+			return tr
 	return FALSE
 
 /// Proc for checking if there is room in the component, returning the amount or else the amount lacking.
@@ -185,7 +201,7 @@
 
 
 /// For consuming a dictionary of materials. mats is the map of materials to use and the corresponding amounts, example: list(M/datum/material/glass =100, datum/material/iron=200)
-/datum/component/material_container/proc/use_materials(list/mats, multiplier=1)
+/datum/component/material_container/proc/use_materials(list/mats, multiplier=1, datum/bank_account/using_account, charge = TRUE)
 	if(!mats || !length(mats))
 		return FALSE
 
@@ -200,22 +216,24 @@
 		var/amount_required = mats[x] * multiplier
 		if(!(materials[req_mat] >= amount_required)) // do we have enough of the resource?
 			return FALSE //Can't afford it
+		if(linked_account && !using_account.has_money(get_material_list_cost(mats, multiplier)))
+			return FALSE //Can't afford the credit cost
 		mats_to_remove[req_mat] += amount_required //Add it to the assoc list of things to remove
 		continue
 
 	var/total_amount_save = total_amount
 
 	for(var/i in mats_to_remove)
-		total_amount_save -= use_amount_mat(mats_to_remove[i], i)
+		total_amount_save -= use_amount_mat(mats_to_remove[i], i, using_account, charge)
 
 	return total_amount_save - total_amount
 
 /// For spawning mineral sheets at a specific location. Used by machines to output sheets.
-/datum/component/material_container/proc/retrieve_sheets(sheet_amt, var/datum/material/M, target = null)
+/datum/component/material_container/proc/retrieve_sheets(sheet_amt, var/datum/material/M, target = null, datum/bank_account/using_account, charge = TRUE)
 	if(!M.sheet_type)
-		return 0 //Add greyscale sheet handling here later
+		return FALSE //Add greyscale sheet handling here later
 	if(sheet_amt <= 0)
-		return 0
+		return FALSE
 
 	if(!target)
 		target = get_turf(parent)
@@ -223,14 +241,16 @@
 		sheet_amt = round(materials[M] / MINERAL_MATERIAL_AMOUNT)
 	var/count = 0
 	while(sheet_amt > MAX_STACK_SIZE)
+		if(!use_amount_mat(sheet_amt * MINERAL_MATERIAL_AMOUNT, M, using_account, charge))
+			break
 		new M.sheet_type(target, MAX_STACK_SIZE)
 		count += MAX_STACK_SIZE
-		use_amount_mat(sheet_amt * MINERAL_MATERIAL_AMOUNT, M)
 		sheet_amt -= MAX_STACK_SIZE
 	if(sheet_amt >= 1)
+		if(!use_amount_mat(sheet_amt * MINERAL_MATERIAL_AMOUNT, M, using_account, charge))
+			return FALSE
 		new M.sheet_type(target, sheet_amt)
 		count += sheet_amt
-		use_amount_mat(sheet_amt * MINERAL_MATERIAL_AMOUNT, M)
 	return count
 
 
@@ -267,6 +287,48 @@
 			return FALSE
 
 	return TRUE
+
+/** For getting the price of a dictionary of materials.
+  *
+  * * mats - A map of materials to use and the corresponding amounts, example: list(M/datum/material/glass =100, datum/material/iron=200)
+  * * multiplier - How many mats lists should be added
+  */
+/datum/component/material_container/proc/get_material_list_cost(list/mats, multiplier=1)
+	if(!mats || !length(mats))
+		return FALSE
+
+	var/cost = 0
+	for(var/x in mats) //Loop through all required materials
+		var/datum/material/req_mat = x
+		cost += get_material_cost(req_mat, mats[x])
+
+	return CEILING(cost, 1)
+
+/** Returns the cost of a certain amount of one material
+  *
+  * * mat - The material to find the value of.
+  * * amt - The amout of material in question.
+  * * round - Whether or not to round the cost. **MORE IMPORTANT THAN IT SEEMS, IT COULD MEAN THE DIFFERENCE BETWEEN 2000cr AND 25cr!!**
+  */
+/datum/component/material_container/proc/get_material_cost(datum/material/mat, amt = 1, round = TRUE)
+	if(GLOB.security_level >= SEC_LEVEL_RED)
+		return FALSE //It's free when it's an emergency, don't want to prevent everyone from helping
+	if(!istype(mat))
+		mat = SSmaterials.GetMaterialRef(mat) //Get the ref if necesary
+
+	var/total_cost = amt * mat.value_per_unit * cost_modifier
+	if(!linked_account)
+		return FALSE //Don't need to charge for it, return default
+	if(!materials[mat]) //Do we have the resource?
+		//Can't afford it, send the default price times five so that it's very profitable to put in
+		return round ? CEILING(total_cost * 5, 1) : total_cost
+
+	var/amt_ratio = (materials[mat] + 1) / amt
+	if (amt_ratio < 2 && amt > 1)
+		// Cost Scaling = 2*(Amount Ratio - 1)^2
+		total_cost *= 1 + ((amt_ratio - 1) ** 2)
+
+	return round ? CEILING(total_cost, 1) : total_cost
 
 /// Returns all the categories in a recipe.
 /datum/component/material_container/proc/get_categories(list/mats)

code/modules/mining/machine_redemption.dm
@@ -18,7 +18,6 @@
 	var/points = 0
 	var/ore_multiplier = 1
 	var/point_upgrade = 1
-	var/list/ore_values = list(/datum/material/iron = 1, /datum/material/glass = 1,  /datum/material/plasma = 15,  /datum/material/silver = 16, /datum/material/gold = 18, /datum/material/titanium = 30, /datum/material/uranium = 30, /datum/material/diamond = 50, /datum/material/bluespace = 50, /datum/material/bananium = 60)
 	/// Variable that holds a timer which is used for callbacks to `send_console_message()`. Used for preventing multiple calls to this proc while the ORM is eating a stack of ores.
 	var/console_notify_timer
 	var/datum/techweb/stored_research
@@ -214,7 +213,9 @@
 
 /obj/machinery/mineral/ore_redemption/ui_data(mob/user)
 	var/list/data = list()
+	var/datum/bank_account/user_account = user.get_bank_account()
 	data["unclaimedPoints"] = points
+	data["userCash"] = user_account.account_balance
 
 	data["materials"] = list()
 	var/datum/component/material_container/mat_container = materials.mat_container
@@ -224,12 +225,12 @@
 			var/amount = mat_container.materials[M]
 			var/sheet_amount = amount / MINERAL_MATERIAL_AMOUNT
 			var/ref = REF(M)
-			data["materials"] += list(list("name" = M.name, "id" = ref, "amount" = sheet_amount, "value" = ore_values[M.type]))
+			data["materials"] += list(list("name" = M.name, "id" = ref, "amount" = sheet_amount, "value" = mat_container.get_material_cost(M, MINERAL_MATERIAL_AMOUNT)))
 
 		data["alloys"] = list()
 		for(var/v in stored_research.researched_designs)
 			var/datum/design/D = SSresearch.techweb_design_by_id(v)
-			data["alloys"] += list(list("name" = D.name, "id" = D.id, "amount" = can_smelt_alloy(D)))
+			data["alloys"] += list(list("name" = D.name, "id" = D.id, "amount" = can_smelt_alloy(D), "value" = mat_container.get_material_list_cost(D.materials)))
 
 	if (!mat_container)
 		data["disconnected"] = "local mineral storage is unavailable"
@@ -269,6 +270,7 @@
 				to_chat(usr, "<span class='warning'>No points to claim.</span>")
 			return TRUE
 		if("Release")
+			var/obj/item/card/id/I = usr.get_idcard(TRUE)
 			if(!mat_container)
 				return
 			if(materials.on_hold())
@@ -295,7 +297,7 @@
 
 				var/sheets_to_remove = round(min(desired,50,stored_amount))
 
-				var/count = mat_container.retrieve_sheets(sheets_to_remove, mat, get_step(src, output_dir))
+				var/count = mat_container.retrieve_sheets(sheets_to_remove, mat, get_step(src, output_dir), I?.registered_account)
 				var/list/mats = list()
 				mats[mat] = MINERAL_MATERIAL_AMOUNT
 				materials.silo_log(src, "released", -count, "sheets", mats)
@@ -330,15 +332,20 @@
 			var/datum/design/alloy = stored_research.isDesignResearchedID(alloy_id)
 			var/mob/M = usr
 			var/obj/item/card/id/I = M.get_idcard(TRUE)
+			var/datum/bank_account/user_account = I?.registered_account
 			if((check_access(I) || allowed(usr)) && alloy)
 				var/smelt_amount = can_smelt_alloy(alloy)
+				if(mat_container.linked_account && !(obj_flags & EMAGGED))
+					var/cost = mat_container.get_material_list_cost(alloy.materials)
+					if(cost)
+						smelt_amount = min(smelt_amount, FLOOR(user_account?.account_balance / cost, 1))
 				var/desired = 0
 				if (params["sheets"])
 					desired = text2num(params["sheets"])
 				else
 					desired = input("How many sheets?", "How many sheets would you like to smelt?", 1) as null|num
 				var/amount = round(min(desired,50,smelt_amount))
-				mat_container.use_materials(alloy.materials, amount)
+				mat_container.use_materials(alloy.materials, amount, user_account, !(obj_flags & EMAGGED))
 				materials.silo_log(src, "released", -amount, "sheets", alloy.materials)
 				var/output
 				if(ispath(alloy.build_path, /obj/item/stack/sheet))

code/modules/research/machinery/_production.dm
@@ -152,8 +152,14 @@
 		if(!reagents.has_reagent(R, D.reagents_list[R]*amount/coeff))
 			say("Not enough reagents to complete prototype[amount > 1? "s" : ""].")
 			return FALSE
-	materials.mat_container.use_materials(efficient_mats, amount)
-	materials.silo_log(src, "built", -amount, "[D.name]", efficient_mats)
+	var/datum/bank_account/user_account = usr.get_bank_account()
+	if(materials.mat_container.linked_account && !(obj_flags & EMAGGED))
+		var/cost = materials.mat_container.get_material_list_cost(efficient_mats)
+		if(!user_account.has_money(cost))
+			say("Insufficient funds to complete prototype[amount > 1? "s" : ""].")
+			return FALSE
+	materials.mat_container.use_materials(efficient_mats, amount, user_account)
+	materials.silo_log(src, "built", -amount, "[D.name]", efficient_mats, !(obj_flags & EMAGGED))
 	for(var/R in D.reagents_list)
 		reagents.remove_reagent(R, D.reagents_list[R]*amount/coeff)
 	busy = TRUE
@@ -197,6 +203,9 @@
 	var/list/l = list()
 	l += "<div class='statusDisplay'><b>[host_research.organization] [department_tag] Department Lathe</b>"
 	l += "Security protocols: [(obj_flags & EMAGGED)? "<font color='red'>Disabled</font>" : "<font color='green'>Enabled</font>"]"
+	if (materials.mat_container.linked_account)
+		var/datum/bank_account/user_account = usr.get_bank_account()
+		l += "<B>User credit balance:</B> [user_account ? user_account.account_balance : "N/A"] cr, <B>Remote balance:</B> [materials.mat_container.linked_account.account_balance] cr"
 	if (materials.mat_container)
 		l += "<A href='?src=[REF(src)];switch_screen=[RESEARCH_FABRICATOR_SCREEN_MATERIALS]'><B>Material Amount:</B> [materials.format_amount()]</A>"
 	else
@@ -215,11 +224,17 @@
 	for(var/mat_id in materials.mat_container.materials)
 		var/datum/material/M = mat_id
 		var/amount = materials.mat_container.materials[mat_id]
+		var/cost = materials.mat_container.get_material_cost(M, MINERAL_MATERIAL_AMOUNT)
+		var/obj/item/card/id/user_id = usr.get_idcard(TRUE)
+		var/account_balance = user_id?.registered_account?.account_balance
 		var/ref = REF(M)
-		l += "* [amount] of [M.name]: "
-		if(amount >= MINERAL_MATERIAL_AMOUNT) l += "<A href='?src=[REF(src)];ejectsheet=[ref];eject_amt=1'>Eject</A> [RDSCREEN_NOBREAK]"
-		if(amount >= MINERAL_MATERIAL_AMOUNT*5) l += "<A href='?src=[REF(src)];ejectsheet=[ref];eject_amt=5'>5x</A> [RDSCREEN_NOBREAK]"
-		if(amount >= MINERAL_MATERIAL_AMOUNT) l += "<A href='?src=[REF(src)];ejectsheet=[ref];eject_amt=50'>All</A>[RDSCREEN_NOBREAK]"
+		l += "* [amount] units of [M.name]: ([cost]cr/sheet) "
+		if(amount >= MINERAL_MATERIAL_AMOUNT && account_balance >= cost)
+			l += "<A href='?src=[REF(src)];ejectsheet=[ref];eject_amt=1'>Eject</A> [RDSCREEN_NOBREAK]"
+		if(amount >= MINERAL_MATERIAL_AMOUNT*5 && account_balance >= cost*5)
+			l += "<A href='?src=[REF(src)];ejectsheet=[ref];eject_amt=5'>5x</A> [RDSCREEN_NOBREAK]"
+		if(amount >= MINERAL_MATERIAL_AMOUNT && account_balance >= cost)
+			l += "<A href='?src=[REF(src)];ejectsheet=[ref];eject_amt=50'>All</A>[RDSCREEN_NOBREAK]"
 		l += ""
 	l += "</div>[RDSCREEN_NOBREAK]"
 	return l
@@ -270,6 +285,21 @@
 			temp_material += " [all_materials[M]/coeff] [CallMaterialName(M)]"
 		c = min(c,t)
 
+	if(materials?.mat_container?.linked_account)
+		var/cost
+		if((obj_flags & EMAGGED) || iscyborg(usr) || isdrone(usr))
+			cost = 0
+		else
+			cost = materials.mat_container.get_material_list_cost(D.materials)
+		if(cost)
+			var/datum/bank_account/user_account = usr.get_bank_account()
+			var/d = FLOOR(user_account?.account_balance / cost, 1)
+			if(d < 1)
+				temp_material += " | <span class='bad'>[cost] cr/Item</span>"
+			else
+				temp_material += " | [cost] cr/Item"
+			c = min(c, d)
+
 	if (c >= 1)
 		l += "<A href='?src=[REF(src)];build=[D.id];amount=1'>[D.name]</A>[RDSCREEN_NOBREAK]"
 		if(c >= 5)
@@ -319,7 +349,8 @@
 	if (materials.on_hold())
 		say("Mineral access is on hold, please contact the quartermaster.")
 		return 0
-	var/count = mat_container.retrieve_sheets(text2num(eject_amt), eject_sheet, drop_location())
+	var/obj/item/card/id/user_id = usr.get_idcard(TRUE)
+	var/count = mat_container.retrieve_sheets(text2num(eject_amt), eject_sheet, drop_location(), user_id.registered_account)
 	var/list/matlist = list()
 	matlist[eject_sheet] = MINERAL_MATERIAL_AMOUNT
 	materials.silo_log(src, "ejected", -count, "sheets", matlist)

triplezeta/ScoundrelOutpost

code/datums/components/material_container.dm
@@ -9,6 +9,8 @@
 		MAX_STACK_SIZE - size of a stack of mineral sheets. Constant.
 */
 
+#define MINIMUM_REFUND_THRESHOLD 5000
+
 /datum/component/material_container
 	/// The total amount of materials this material container contains
 	var/total_amount = 0
@@ -30,6 +32,12 @@
 	var/datum/callback/precondition
 	/// A callback invoked after materials are inserted into this container
 	var/datum/callback/after_insert
+	/// The bank account that will recieve funds at certain times when mats are used/withdrawn.
+	var/datum/bank_account/linked_account
+	/// Modifier for all materials drawn when there's a linked account.
+	var/cost_modifier = 1
+	/// The minimum required amount of credits in a bank account to consider giving refunds for inserting materials. Currently the same as cargo gets roundstart.
+	var/refund_minimum = 5000
 	/// The material container flags. See __DEFINES/materials.dm.
 	var/mat_container_flags
 
@@ -165,6 +173,12 @@
 	var/inserted = insert_item(held_item, stack_amt = requested_amount, breakdown_flags= mat_container_flags)
 	if(inserted)
 		to_chat(user, span_notice("You insert a material total of [inserted] into [parent]."))
+		if(linked_account)
+			var/cost = get_material_list_cost(held_item.custom_materials) * (cost_modifier / 4) //25% refund default, scaling with the price of mats.
+			cost = FLOOR(clamp(cost, 0, linked_account.account_balance - refund_minimum), 1) //Will pay what it can, as long as it doesn't go under the minimum threshold.
+			var/obj/item/card/id/card = user.get_idcard(TRUE)
+			if(card?.registered_account)
+				card.registered_account.transfer_money(linked_account, cost, "Material Refund")
 		qdel(held_item)
 		if(after_insert)
 			after_insert.Invoke(held_item, last_inserted_id, inserted)
@@ -247,7 +261,7 @@
 	return (total_amount - total_amount_saved)
 
 /// Uses an amount of a specific material, effectively removing it.
-/datum/component/material_container/proc/use_amount_mat(amt, datum/material/mat)
+/datum/component/material_container/proc/use_amount_mat(amt, var/datum/material/mat, datum/bank_account/using_account, charge = FALSE)
 	if(!istype(mat))
 		mat = GET_MATERIAL_REF(mat)
 
@@ -256,7 +270,8 @@
 	var/amount = materials[mat]
 	if(amount < amt)
 		return 0
-
+	if(charge && linked_account && !linked_account.transfer_money(using_account, get_material_cost(mat, amt), "Printed Item with Ore Silo Materials")) // could make the transaction reason include what item is printed?
+		return 0
 	materials[mat] -= amt
 	total_amount -= amt
 	return amt
@@ -271,9 +286,9 @@
 		return T.transer_amt_to(src, -amt, mat)
 	var/tr = min(amt, materials[mat], T.can_insert_amount_mat(amt, mat))
 	if(tr)
-		use_amount_mat(tr, mat)
-		T.insert_amount_mat(tr, mat)
-		return tr
+		if(use_amount_mat(tr, mat))
+			T.insert_amount_mat(tr, mat)
+			return tr
 	return FALSE
 
 /// Proc for checking if there is room in the component, returning the amount or else the amount lacking.
@@ -288,7 +303,7 @@
 
 
 /// For consuming a dictionary of materials. mats is the map of materials to use and the corresponding amounts, example: list(M/datum/material/glass =100, datum/material/iron=200)
-/datum/component/material_container/proc/use_materials(list/mats, multiplier=1)
+/datum/component/material_container/proc/use_materials(list/mats, multiplier=1, datum/bank_account/using_account, charge = TRUE)
 	if(!mats || !length(mats))
 		return FALSE
 
@@ -305,22 +320,24 @@
 			return FALSE //No negative mats
 		if(!(materials[req_mat] >= amount_required)) // do we have enough of the resource?
 			return FALSE //Can't afford it
+		if(linked_account && !using_account.has_money(get_material_list_cost(mats, multiplier)))
+			return FALSE //Can't afford the credit cost
 		mats_to_remove[req_mat] += amount_required //Add it to the assoc list of things to remove
 		continue
 
 	var/total_amount_save = total_amount
 
 	for(var/i in mats_to_remove)
-		total_amount_save -= use_amount_mat(mats_to_remove[i], i)
+		total_amount_save -= use_amount_mat(mats_to_remove[i], i, using_account, charge)
 
 	return total_amount_save - total_amount
 
 /// For spawning mineral sheets at a specific location. Used by machines to output sheets.
-/datum/component/material_container/proc/retrieve_sheets(sheet_amt, datum/material/M, atom/target = null)
+/datum/component/material_container/proc/retrieve_sheets(sheet_amt, datum/material/M, atom/target = null, datum/bank_account/using_account, charge = TRUE)
 	if(!M.sheet_type)
-		return 0 //Add greyscale sheet handling here later
+		return FALSE //Add greyscale sheet handling here later
 	if(sheet_amt <= 0)
-		return 0
+		return FALSE
 
 	if(!target)
 		var/atom/parent_atom = parent
@@ -329,14 +346,16 @@
 		sheet_amt = round(materials[M] / MINERAL_MATERIAL_AMOUNT)
 	var/count = 0
 	while(sheet_amt > MAX_STACK_SIZE)
+		if(!use_amount_mat(sheet_amt * MINERAL_MATERIAL_AMOUNT, M, using_account, charge))
+			break
 		new M.sheet_type(target, MAX_STACK_SIZE, null, list((M) = MINERAL_MATERIAL_AMOUNT))
 		count += MAX_STACK_SIZE
-		use_amount_mat(sheet_amt * MINERAL_MATERIAL_AMOUNT, M)
 		sheet_amt -= MAX_STACK_SIZE
 	if(sheet_amt >= 1)
+		if(!use_amount_mat(sheet_amt * MINERAL_MATERIAL_AMOUNT, M, using_account, charge))
+			return FALSE
 		new M.sheet_type(target, sheet_amt, null, list((M) = MINERAL_MATERIAL_AMOUNT))
 		count += sheet_amt
-		use_amount_mat(sheet_amt * MINERAL_MATERIAL_AMOUNT, M)
 	return count
 
 
@@ -373,6 +392,48 @@
 			return FALSE
 
 	return TRUE
+
+/** For getting the price of a dictionary of materials.
+  *
+  * * mats - A map of materials to use and the corresponding amounts, example: list(M/datum/material/glass =100, datum/material/iron=200)
+  * * multiplier - How many mats lists should be added
+  */
+/datum/component/material_container/proc/get_material_list_cost(list/mats, multiplier=1)
+	if(!mats || !length(mats))
+		return FALSE
+
+	var/cost = 0
+	for(var/x in mats) //Loop through all required materials
+		var/datum/material/req_mat = x
+		cost += get_material_cost(req_mat, mats[x])
+
+	return CEILING(cost, 1)
+
+/** Returns the cost of a certain amount of one material
+  *
+  * * mat - The material to find the value of.
+  * * amt - The amout of material in question.
+  * * round - Whether or not to round the cost. **MORE IMPORTANT THAN IT SEEMS, IT COULD MEAN THE DIFFERENCE BETWEEN 2000cr AND 25cr!!**
+  */
+/datum/component/material_container/proc/get_material_cost(datum/material/mat, amt = 1, round = TRUE)
+	if(GLOB.security_level >= SEC_LEVEL_RED)
+		return FALSE //It's free when it's an emergency, don't want to prevent everyone from helping
+	if(!istype(mat))
+		mat = SSmaterials.GetMaterialRef(mat) //Get the ref if necesary
+
+	var/total_cost = amt * mat.value_per_unit * cost_modifier
+	if(!linked_account)
+		return FALSE //Don't need to charge for it, return default
+	if(!materials[mat]) //Do we have the resource?
+		//Can't afford it, send the default price times five so that it's very profitable to put in
+		return round ? CEILING(total_cost * 5, 1) : total_cost
+
+	var/amt_ratio = (materials[mat] + 1) / amt
+	if (amt_ratio < 2 && amt > 1)
+		// Cost Scaling = 2*(Amount Ratio - 1)^2
+		total_cost *= 1 + ((amt_ratio - 1) ** 2)
+
+	return round ? CEILING(total_cost, 1) : total_cost
 
 /// Returns all the categories in a recipe.
 /datum/component/material_container/proc/get_categories(list/mats)

code/modules/mining/machine_redemption.dm
@@ -18,7 +18,6 @@
 	var/points = 0
 	var/ore_multiplier = 1
 	var/point_upgrade = 1
-	var/list/ore_values = list(/datum/material/iron = 1, /datum/material/glass = 1,  /datum/material/plasma = 15,  /datum/material/silver = 16, /datum/material/gold = 18, /datum/material/titanium = 30, /datum/material/uranium = 30, /datum/material/diamond = 50, /datum/material/bluespace = 50, /datum/material/bananium = 60)
 	/// Variable that holds a timer which is used for callbacks to `send_console_message()`. Used for preventing multiple calls to this proc while the ORM is eating a stack of ores.
 	var/console_notify_timer
 	var/datum/techweb/stored_research
@@ -207,7 +206,9 @@
 
 /obj/machinery/mineral/ore_redemption/ui_data(mob/user)
 	var/list/data = list()
+	var/datum/bank_account/user_account = user.get_bank_account()
 	data["unclaimedPoints"] = points
+	data["userCash"] = user_account.account_balance
 
 	data["materials"] = list()
 	var/datum/component/material_container/mat_container = materials.mat_container
@@ -217,12 +218,12 @@
 			var/amount = mat_container.materials[M]
 			var/sheet_amount = amount / MINERAL_MATERIAL_AMOUNT
 			var/ref = REF(M)
-			data["materials"] += list(list("name" = M.name, "id" = ref, "amount" = sheet_amount, "value" = ore_values[M.type]))
+			data["materials"] += list(list("name" = M.name, "id" = ref, "amount" = sheet_amount, "value" = mat_container.get_material_cost(M, MINERAL_MATERIAL_AMOUNT)))
 
 		data["alloys"] = list()
 		for(var/v in stored_research.researched_designs)
 			var/datum/design/D = SSresearch.techweb_design_by_id(v)
-			data["alloys"] += list(list("name" = D.name, "id" = D.id, "amount" = can_smelt_alloy(D)))
+			data["alloys"] += list(list("name" = D.name, "id" = D.id, "amount" = can_smelt_alloy(D), "value" = mat_container.get_material_list_cost(D.materials)))
 
 	if (!mat_container)
 		data["disconnected"] = "local mineral storage is unavailable"
@@ -264,6 +265,7 @@
 				to_chat(usr, span_warning("No points to claim."))
 			return TRUE
 		if("Release")
+			var/obj/item/card/id/I = usr.get_idcard(TRUE)
 			if(!mat_container)
 				return
 			if(materials.on_hold())
@@ -291,7 +293,7 @@
 						return
 				var/sheets_to_remove = round(min(desired,50,stored_amount))
 
-				var/count = mat_container.retrieve_sheets(sheets_to_remove, mat, get_step(src, output_dir))
+				var/count = mat_container.retrieve_sheets(sheets_to_remove, mat, get_step(src, output_dir), I?.registered_account)
 				var/list/mats = list()
 				mats[mat] = MINERAL_MATERIAL_AMOUNT
 				materials.silo_log(src, "released", -count, "sheets", mats)
@@ -325,11 +327,16 @@
 			var/alloy_id = params["id"]
 			var/datum/design/alloy = stored_research.isDesignResearchedID(alloy_id)
 			var/obj/item/card/id/user_id_card
+			var/datum/bank_account/user_account = user_id_card?.registered_account
 			if(isliving(usr))
 				var/mob/living/user = usr
 				user_id_card = user.get_idcard(TRUE)
 			if((check_access(user_id_card) || allowed(usr)) && alloy)
 				var/smelt_amount = can_smelt_alloy(alloy)
+				if(mat_container.linked_account && !(obj_flags & EMAGGED))
+					var/cost = mat_container.get_material_list_cost(alloy.materials)
+					if(cost)
+						smelt_amount = min(smelt_amount, FLOOR(user_account?.account_balance / cost, 1))
 				var/desired = 0
 				if (params["sheets"])
 					desired = text2num(params["sheets"])

code/modules/research/machinery/_production.dm
@@ -319,8 +319,14 @@
 
 		borg.cell.use(SILICON_LATHE_TAX)
 
-	materials.mat_container.use_materials(efficient_mats, print_quantity)
-	materials.silo_log(src, "built", -print_quantity, "[design.name]", efficient_mats)
+	var/datum/bank_account/user_account = usr.get_bank_account()
+	if(materials.mat_container.linked_account && !(obj_flags & EMAGGED))
+		var/cost = materials.mat_container.get_material_list_cost(efficient_mats)
+		if(!user_account.has_money(cost))
+			say("Insufficient funds to complete prototype[amount > 1? "s" : ""].")
+			return FALSE
+	materials.mat_container.use_materials(efficient_mats, print_quantity, user_account)
+	materials.silo_log(src, "built", -print_quantity, "[design.name]", efficient_mats, !(obj_flags & EMAGGED))
 
 	for(var/reagent in design.reagents_list)
 		reagents.remove_reagent(reagent, design.reagents_list[reagent] * print_quantity * coefficient)
@@ -347,9 +353,8 @@
 	if(materials.on_hold())
 		say("Mineral access is on hold, please contact the quartermaster.")
 		return 0
-
-	var/count = mat_container.retrieve_sheets(text2num(eject_amt), eject_sheet, drop_location())
-
+	var/obj/item/card/id/user_id = usr.get_idcard(TRUE)
+	var/count = mat_container.retrieve_sheets(text2num(eject_amt), eject_sheet, drop_location(), user_id.registered_account)
 	var/list/matlist = list()
 	matlist[eject_sheet] = MINERAL_MATERIAL_AMOUNT
 